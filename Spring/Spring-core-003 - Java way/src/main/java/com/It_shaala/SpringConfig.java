79b1212785431d49ae6b6499565317aeb23256e5
Rahul Khilari <149717670+Rahul0792@users.noreply.github.com>
2025-09-03 11:46:09 +0530

    spring - java way

---
+++ Spring/Spring-core-003 - Java way/src/main/java/com/It_shaala/SpringConfig.java
@@ -0,0 +1,16 @@
+package com.It_shaala;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.It_shaala.Course"})
+public class SpringConfig {
+
+    @Bean(name = "course")
+    public Course getCourse(){
+        return new Course(1,"Python","Hard");
+    }
+
+}
